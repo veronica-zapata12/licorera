a41a6afcc15733a80e673486a67a2516a1b1feb7
veronica-zapata12 <veritozapata12@gmail.com>
2019-11-27 15:33:09 -0500

    test infraestructura facura

--- licorera/src/test/java/com/ceiba/licorera/TestInfraestructura/controladorFactura/ControladorFacturaTest.java
+++ licorera/src/test/java/com/ceiba/licorera/TestInfraestructura/controladorFactura/ControladorFacturaTest.java
@@ -8,11 +8,13 @@ import com.ceiba.licorera.dominio.modelo.Producto;
 import com.ceiba.licorera.dominio.modelo.dto.DetallefacturaDto;
 import com.ceiba.licorera.dominio.modelo.dto.FacturaDto;
 import com.ceiba.licorera.dominio.modelo.dto.ProductoDto;
+import com.ceiba.licorera.infraestructura.repositorio.DetalleFacturaRepositorio;
 import com.ceiba.licorera.infraestructura.repositorio.FacturaRepositorio;
 import com.ceiba.licorera.infraestructura.repositorio.ProductoRepositorio;
 import com.ceiba.licorera.infraestructura.repositorio.adaptador.AdaptadorDetalleFacturaJpa;
 import com.ceiba.licorera.infraestructura.repositorio.adaptador.AdaptadorFacturaJpa;
 import com.ceiba.licorera.infraestructura.repositorio.adaptador.AdaptadorProductoJpa;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -53,7 +55,7 @@ public class ControladorFacturaTest {
     private AdaptadorDetalleFacturaJpa adaptadorDetalleFacturaJpa;
     @Autowired
     private ProductoRepositorio productoRepositorio;
-
+   
     @Before
     public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
@@ -76,40 +78,76 @@ public class ControladorFacturaTest {
     @Test
     void listarFacturas() throws Exception {
         //arrange
-        AdaptadorProductoJpa adaptadorProductoJpa= new AdaptadorProductoJpa(productoRepositorio);
-        AdaptadorFacturaJpa adaptadorFacturaJpa = new AdaptadorFacturaJpa(facturaRepositorio,adaptadorDetalleFacturaJpa);
-        List<FacturaDto> facturaDtoList =new ArrayList<>();
+        AdaptadorProductoJpa adaptadorProductoJpa = new AdaptadorProductoJpa(productoRepositorio);
+        AdaptadorFacturaJpa adaptadorFacturaJpa = new AdaptadorFacturaJpa(facturaRepositorio, adaptadorDetalleFacturaJpa);
+        List<FacturaDto> facturaDtoList = new ArrayList<>();
         List<DetalleFactura> detalleFacturas = new ArrayList<>();
-        Long id = 1L;
-        Producto producto1 = new Producto( 1L,"vodka", 55000.0);
+    Long id = 1L;
+    Producto producto1 = new Producto(1L, "vodka", 55000.0);
         adaptadorProductoJpa.crear(producto1);
-        int cantidad1 = 3;
-        DetalleFactura detalleFactura = new DetalleFactura(cantidad1, producto1);
+    int cantidad1 = 3;
+    DetalleFactura detalleFactura = new DetalleFactura(cantidad1, producto1);
         detalleFacturas.add(detalleFactura);
-        Producto producto2 = new Producto( 1L,"ron", 40000.0);
+    Producto producto2 = new Producto(1L, "ron", 40000.0);
         adaptadorProductoJpa.crear(producto2);
-        int cantidad2 = 4;
-        DetalleFactura detalleFactura2 = new DetalleFactura(cantidad2, producto2);
+    int cantidad2 = 4;
+    DetalleFactura detalleFactura2 = new DetalleFactura(cantidad2, producto2);
         detalleFacturas.add(detalleFactura2);
-        Factura factura = new Factura(id,detalleFacturas);
+    Factura factura = new Factura(id, detalleFacturas);
         adaptadorFacturaJpa.crear(factura);
-        List<DetallefacturaDto> detalleFacturasDto = new ArrayList<>();
-        Long id1 = 1L;
-        ProductoDto productoDto1 = new ProductoDto( 1L,"vodka", 55000.0);
-        int cantidadDto1 = 3;
-        DetallefacturaDto detalleFacturaDto = new DetallefacturaDto(cantidadDto1, productoDto1);
+    List<DetallefacturaDto> detalleFacturasDto = new ArrayList<>();
+    Long id1 = 1L;
+    ProductoDto productoDto1 = new ProductoDto(1L, "vodka", 55000.0);
+    int cantidadDto1 = 3;
+    DetallefacturaDto detalleFacturaDto = new DetallefacturaDto(cantidadDto1, productoDto1);
         detalleFacturasDto.add(detalleFacturaDto);
-        ProductoDto productoDto2 = new ProductoDto( 1L,"ron", 40000.0);
-        int cantidadDto2 = 4;
-        DetallefacturaDto detalleFacturaDto2 = new DetallefacturaDto(cantidadDto2, productoDto2);
+    ProductoDto productoDto2 = new ProductoDto(1L, "ron", 40000.0);
+    int cantidadDto2 = 4;
+    DetallefacturaDto detalleFacturaDto2 = new DetallefacturaDto(cantidadDto2, productoDto2);
         detalleFacturasDto.add(detalleFacturaDto2);
 
-        FacturaDto facturaDto = new FacturaDto(id1,detalleFacturasDto);
+    FacturaDto facturaDto = new FacturaDto(id1, detalleFacturasDto);
         facturaDtoList.add(facturaDto);
         mockMvc.perform(get("http://localhost:8080/factura")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(facturaDtoList)))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
+            .andDo(print())
+            .andExpect(status().isOk());
+}
+@Test
+    void detalleFacturaPorId() throws Exception {
+    //arrange
+    AdaptadorProductoJpa adaptadorProductoJpa = new AdaptadorProductoJpa(productoRepositorio);
+    AdaptadorFacturaJpa adaptadorFacturaJpa = new AdaptadorFacturaJpa(facturaRepositorio, adaptadorDetalleFacturaJpa);
+    List<DetalleFactura> detalleFacturas = new ArrayList<>();
+    Long id = 1L;
+    Producto producto1 = new Producto(1L, "vodka", 55000.0);
+    adaptadorProductoJpa.crear(producto1);
+    int cantidad1 = 3;
+    DetalleFactura detalleFactura = new DetalleFactura(cantidad1, producto1);
+    detalleFacturas.add(detalleFactura);
+    Producto producto2 = new Producto(1L, "ron", 40000.0);
+    adaptadorProductoJpa.crear(producto2);
+    int cantidad2 = 4;
+    DetalleFactura detalleFactura2 = new DetalleFactura(cantidad2, producto2);
+    detalleFacturas.add(detalleFactura2);
+    Factura factura = new Factura(id, detalleFacturas);
+    adaptadorFacturaJpa.crear(factura);
+    List<DetallefacturaDto> detalleFacturasDto = new ArrayList<>();
+    ProductoDto productoDto1 = new ProductoDto(1L, "vodka", 55000.0);
+    int cantidadDto1 = 3;
+    DetallefacturaDto detalleFacturaDto = new DetallefacturaDto(cantidadDto1, productoDto1);
+    detalleFacturasDto.add(detalleFacturaDto);
+    ProductoDto productoDto2 = new ProductoDto(1L, "ron", 40000.0);
+    int cantidadDto2 = 4;
+    DetallefacturaDto detalleFacturaDto2 = new DetallefacturaDto(cantidadDto2, productoDto2);
+    detalleFacturasDto.add(detalleFacturaDto2);
+
+    mockMvc.perform(get("http://localhost:8080/detallefactura/1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(detalleFacturasDto)))
+            .andDo(print())
+            .andExpect(status().isOk());
+
+}
 }
